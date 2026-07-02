f585963fd6b658078cd2526bcc6f57de0f144a7c
rsky <su@rsky.net>
2026-07-03 01:45:27 +0800

    just so so

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ var (
 func GetConfig() *Config {
 	once.Do(func() {
 		instance = &Config{
-			Proxy:         true,
+			Proxy:         false,
 			ProxyAddress:  "http://127.0.0.1:9999",
 			ClientAddress: "https://wns.rsky.net",
 			// ClientAddress: "http://127.0.0.1:5173",
